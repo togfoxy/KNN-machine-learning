dd1027d45e7ab5dda65242534eb46ef1eaca53d0
Fox <togfoxy@users.noreply.github.com>
2022-12-10 00:54:29 +1000

    Add files via upload

--- main.lua
+++ main.lua
@@ -4,90 +4,101 @@ cf = require 'commonfunctions'
 
 dataset = {}
 
-function loadDataset()
-	table.insert(dataset, {158,58,"M", 0})
-	table.insert(dataset, {158,59,"M", 0})
-	table.insert(dataset, {158,63,"M", 0})
-	table.insert(dataset, {160,59,"M", 0})
-	table.insert(dataset, {160,60,"M", 0})
-	table.insert(dataset, {163,60,"M", 0})
-	table.insert(dataset, {163,61,"M", 0})
-	table.insert(dataset, {160,64,"L", 0})
-	table.insert(dataset, {163,64,"L", 0})
-	table.insert(dataset, {165,61,"L", 0})
-	table.insert(dataset, {165,62,"L", 0})
-	table.insert(dataset, {165,65,"L", 0})
-	table.insert(dataset, {168,62,"L", 0})
-	table.insert(dataset, {168,63,"L", 0})
-	table.insert(dataset, {168,66,"L", 0})
-	table.insert(dataset, {170,63,"L", 0})
-	table.insert(dataset, {170,64,"L", 0})
-	table.insert(dataset, {170,65,"L", 0})
+function ParseCSVLine (line,sep)
+	local res = {}
+	local pos = 1
+	sep = sep or ','
+	while true do
+		local c = string.sub(line,pos,pos)
+		if (c == "") then break end
+		if (c == '"') then
+			-- quoted value (ignore separator within)
+			local txt = ""
+			repeat
+				local startp,endp = string.find(line,'^%b""',pos)
+				txt = txt..string.sub(line,startp+1,endp-1)
+				pos = endp + 1
+				c = string.sub(line,pos,pos)
+				if (c == '"') then txt = txt..'"' end
+				-- check first char AFTER quoted string, if it is another
+				-- quoted string without separator, then append it
+				-- this is the way to "escape" the quote char in a quote. example:
+				--   value1,"blub""blip""boing",value3  will result in blub"blip"boing  for the middle
+			until (c ~= '"')
+			table.insert(res,txt)
+			assert(c == sep or c == "")
+			pos = pos + 1
+		else
+			-- no quotes used, just look for the first separator
+			local startp,endp = string.find(line,sep,pos)
+			if (startp) then
+				table.insert(res,string.sub(line,pos,startp-1))
+				pos = endp + 1
+			else
+				-- no separator found -> use rest of string and terminate
+				table.insert(res,string.sub(line,pos))
+				break
+			end
+		end
+	end
+	return res
 end
 
--- -- normalise the data, including the query data
--- col1min = 999
--- col2min = 999
--- col1max = 0
--- col2max = 0
+-- get all lines from a file, returns an empty
+-- list/table if the file does not exist
+function lines_from(file)
+	-- if not file_exists(file) then return {} end
+	local lines = {}
+	for line in io.lines(file) do
+		local thisline = ParseCSVLine (line,",")
+		if #thisline > 0 then	-- this will trim blank lines
+			lines[#lines + 1] = ParseCSVLine (line,",")
+		end
+	end
+	return lines
+end
 
--- -- get min/max for dataset
--- for i = 1, #dataset do
-    -- if dataset[i][1] > col1max then col1max = dataset[i][1] end
-    -- if dataset[i][1] < col1min then col1min = dataset[i][1] end
-
-    -- if dataset[i][2] > col2max then col2max = dataset[i][2] end
-    -- if dataset[i][2] < col2min then col2min = dataset[i][2] end
+-- function loadDataset()
+-- 	-- table.insert(dataset, {158,58,"M", 0})
+-- 	-- table.insert(dataset, {158,59,"M", 0})
+-- 	-- table.insert(dataset, {158,63,"M", 0})
+-- 	-- table.insert(dataset, {160,59,"M", 0})
+-- 	-- table.insert(dataset, {160,60,"M", 0})
+-- 	-- table.insert(dataset, {163,60,"M", 0})
+-- 	-- table.insert(dataset, {163,61,"M", 0})
+-- 	-- table.insert(dataset, {160,64,"L", 0})
+-- 	-- table.insert(dataset, {163,64,"L", 0})
+-- 	-- table.insert(dataset, {165,61,"L", 0})
+-- 	-- table.insert(dataset, {165,62,"L", 0})
+-- 	-- table.insert(dataset, {165,65,"L", 0})
+-- 	-- table.insert(dataset, {168,62,"L", 0})
+-- 	-- table.insert(dataset, {168,63,"L", 0})
+-- 	-- table.insert(dataset, {168,66,"L", 0})
+-- 	-- table.insert(dataset, {170,63,"L", 0})
+-- 	-- table.insert(dataset, {170,64,"L", 0})
+-- 	-- table.insert(dataset, {170,65,"L", 0})
+--
+-- 	table.insert(dataset, {5.1,3.5,1.4,0.2,"setosa"})
+-- 	table.insert(dataset, {4.9,3.0,1.4,0.2,"setosa"})
+-- 	table.insert(dataset, {4.7,3.2,1.3,0.2,"setosa"})
+-- 	table.insert(dataset, {4.6,3.1,1.5,0.2,"num"})
+-- 	table.insert(dataset, {5.0,3.6,1.4,0.2,"setosa"})
 -- end
--- -- normalise the dataset
--- for i = 1, #dataset do
-    -- dataset[i][1] =cf.round( (dataset[i][1] - col1min) / (col1max - col1min) , 2)
-    -- dataset[i][2] =cf.round( (dataset[i][2] - col2min) / (col2max - col2min) , 2)
--- end
 
--- val1 = 161
--- val2 = 61
+dataset = {}
+-- loadDataset()
+dataset = lines_from("iris.csv")
+inputset = {4.7,3.2,1.3,0.2}
 
--- val1normalised = cf.round( (val1 - col1min) / (col1max - col1min) , 2)
--- val2normalised = cf.round( (val2 - col2min) / (col2max - col2min) , 2)
-
+-- print("Dataset:")
 -- print(inspect(dataset))
-
--- for i = 1, #dataset do
-    -- local dist = cf.getDistance(val1normalised, val2normalised, dataset[i][1], dataset[i][2])
-    -- dataset[i][4] = cf.round(dist, 1)
--- end
-
--- table.sort(dataset, function(k1, k2) return k1[4] < k2[4] end)
-
--- result = {}
--- result["L"] = 0
--- result["M"] = 0
--- k = 5
-
--- for i = 1, k do
-    -- print(dataset[i][1], dataset[i][2], dataset[i][3], dataset[i][4])
-    -- if dataset[i][3] == "L" then
-        -- result["L"] = result["L"] + 1
-    -- else
-        -- result["M"] = result["M"] + 1
-    -- end
--- end
-
--- print("Answer")
--- print(result["M"], result["L"])
-
--- error()
-
--- ************************************************
-
-loadDataset()
+-- print("^^^^^^^^^^^^")
 
 -- assumes a standard format of x predictors and then the label and then a new column will be applied for distance calculations
-numofpredictors = 2
+numofpredictors = #inputset
 labelcolumn = numofpredictors + 1
 distcolumn = labelcolumn + 1
-k = 5
+k = 15
 
 -- get the min/max for each predictor
 colmin = {}
@@ -101,11 +112,14 @@ end
 for i = 1, #dataset do
 	for j = 1, numofpredictors do
 		local predictor = dataset[i][j]
-		if colmin[j] == nil then colmin[j] = predictor end
-		if colmax[j] == nil then colmax[j] = predictor end
-
-		if predictor > colmax[j] then colmax[j] = predictor end
-		if predictor < colmin[j] then colmin[j] = predictor end
+		if predictor == nil then
+			print("Nil predictor detected.")
+		else
+			if colmin[j] == nil then colmin[j] = predictor end
+			if colmax[j] == nil then colmax[j] = predictor end
+			if predictor > colmax[j] then colmax[j] = predictor end
+			if predictor < colmin[j] then colmin[j] = predictor end
+		end
 	end
 end
 
@@ -118,20 +132,23 @@ end
 
 -- normalise the input
 -- if numofpredictors = x then the input needs to have x features/predictors
-inputset = {161, 61}	--! might need to preserve this and copy to a new table
+print("Normalised input:")
 for j = 1, numofpredictors do
 	inputset[j] = cf.round((inputset[j] - colmin[j]) / (colmax[j] - colmin[j]), 2)
+	print(inputset[j])
 end
 
 -- compute distance between input and every dataset item
 for i = 1, #dataset do
 	local dist = cf.getDistanceV(inputset, dataset[i])
 	dataset[i][distcolumn] = cf.round(dist, 2)		-- store the distance in the last column
+-- print(dist)
 end
 
 -- sort the table based on the last column (distance)
 table.sort(dataset, function(k1, k2) return k1[distcolumn] < k2[distcolumn] end)
 
+print("Sorted dataset with distance")
 print(inspect(dataset))
 
 -- can now determine result
@@ -143,14 +160,14 @@ for i = 1, k do
 	result[label] = result[label] + 1
 end
 
-print("*********")
-print(inspect(result))
+-- print("*********")
+-- print(inspect(result))
 
 -- determine which label got the most 'hits'
 table.sort(result, function(a, b) return a[1] < b[1] end)
 
-print("*********")
-print(inspect(result))
+-- print("*********")
+-- print(inspect(result))
 
 -- here is the prediction
 print("************")
