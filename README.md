ef793f535a4014dd5342f8802c4d1a3fa9d99991
Fox <togfoxy@users.noreply.github.com>
2022-12-11 11:05:06 +1000

    Ready for publication
    Ready for publication

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # KNN-machine-learning
+
+This is a Lua (love2d) implementation of KNN algorithm often used in supervised machine learning. This code currently does categorisation only and not regression.
+
+See main.lua for an example of how to use.
+
+Assumptions
+===========
+
+The dataset is in a CSV file in the same folder as main.lua but this can be changed if you provide a file path (untested).
+
+CSV format example:
+
+6.3,2.7,4.9,1.8,Iris-virginica
+
+The dataset can have any number of predictors or features. This code assumes the last item is the classifier/label.
+
+Output
+======
+The KNN function returns two variables:
+
+- the classifier/label that has the strongest association with the input set
+
+- the count or number of instances of that classifier was associated with the input set
+
+MIT licence
+Contact me for bugs or defects or suggestions for improvements
---
+++ lib/knn.lua
@@ -0,0 +1,185 @@
+knn = {}
+
+local k = 3     -- default value
+
+function round(num, idp)
+	--Input: number to round; decimal places required
+	assert(num ~= nil, "Can't ROUND a nil value")
+	return tonumber(string.format("%." .. (idp or 0) .. "f", num))
+end
+
+function getDistanceV(inputset, datapoint)
+	-- accepts a variable number of arguments reflecting 2 datapoints on multiple axis (e.g. x,y,z and q,w,e)
+	-- NOTE: multiple axis does not mean multiple datapoints! There are still only two datapoints.
+	-- inputset eg {x,y,z}
+	-- datapoint eg {q,w,e}
+
+	local numaxis = #inputset
+
+	-- split the args into each of the axis, remembering there are only two datapoints expressed across multiple axis
+	local axis = {}
+	for i = 1, numaxis do
+		axis[i] = (inputset[1] - datapoint[1])^2
+	end
+
+	-- sum all the axis and then root it
+	local total = 0
+	for i = 1, #axis do
+		total = total + axis[i]
+	end
+	return math.sqrt(total)
+end
+
+function knn.ParseCSVLine (line,sep)
+	local res = {}
+	local pos = 1
+	sep = sep or ','
+	while true do
+		local c = string.sub(line,pos,pos)
+		if (c == "") then break end
+		if (c == '"') then
+			-- quoted value (ignore separator within)
+			local txt = ""
+			repeat
+				local startp,endp = string.find(line,'^%b""',pos)
+				txt = txt..string.sub(line,startp+1,endp-1)
+				pos = endp + 1
+				c = string.sub(line,pos,pos)
+				if (c == '"') then txt = txt..'"' end
+				-- check first char AFTER quoted string, if it is another
+				-- quoted string without separator, then append it
+				-- this is the way to "escape" the quote char in a quote. example:
+				--   value1,"blub""blip""boing",value3  will result in blub"blip"boing  for the middle
+			until (c ~= '"')
+			table.insert(res,txt)
+			assert(c == sep or c == "")
+			pos = pos + 1
+		else
+			-- no quotes used, just look for the first separator
+			local startp,endp = string.find(line,sep,pos)
+			if (startp) then
+				table.insert(res,string.sub(line,pos,startp-1))
+				pos = endp + 1
+			else
+				-- no separator found -> use rest of string and terminate
+				table.insert(res,string.sub(line,pos))
+				break
+			end
+		end
+	end
+	return res
+end
+
+-- get all lines from a file, returns an empty
+-- list/table if the file does not exist
+function knn.readFromCSV(file)
+	-- if not file_exists(file) then return {} end
+	local lines = {}
+	for line in io.lines(file) do
+		local thisline = knn.ParseCSVLine (line,",")
+		if #thisline > 0 then	-- this will trim blank lines
+			lines[#lines + 1] = knn.ParseCSVLine (line,",")
+		end
+	end
+	return lines
+end
+
+function knn.getPrediction(inputset, dataset, kvalue)
+    local k = kvalue
+    -- assumes a standard format of x predictors and then the label and then a new column will be applied for distance calculations
+    -- the new column does not need to be passed in
+    local numofpredictors = #inputset
+    local labelcolumn = numofpredictors + 1
+    local distcolumn = labelcolumn + 1
+
+    -- get the min/max for each predictor
+    local colmin = {}
+    local colmax = {}
+
+    -- initialise the min/max
+    for i = 1, numofpredictors do
+    	colmin[i] = nil		-- eg colmin[1] = 999; colmin[2] = 999 etc
+    	colmax[i] = nil
+    end
+
+    -- determine min/max
+    for i = 1, #dataset do
+    	for j = 1, numofpredictors do
+    		local predictor = dataset[i][j]
+    		if predictor == nil then
+    			print("Nil predictor detected.")
+    		else
+    			if colmin[j] == nil then colmin[j] = predictor end
+    			if colmax[j] == nil then colmax[j] = predictor end
+    			if predictor > colmax[j] then colmax[j] = predictor end
+    			if predictor < colmin[j] then colmin[j] = predictor end
+    		end
+    	end
+    end
+
+    -- normalise the dataset
+    for i = 1, #dataset do
+    	for j = 1, numofpredictors do
+    		dataset[i][j] = round((dataset[i][j] - colmin[j]) / (colmax[j] - colmin[j]), 2)
+    	end
+    end
+
+    -- normalise the input
+    -- if numofpredictors = x then the input needs to have x features/predictors
+    -- print("Normalised input:")
+    for j = 1, numofpredictors do
+    	inputset[j] = round((inputset[j] - colmin[j]) / (colmax[j] - colmin[j]), 2)
+    	-- print(inputset[j])
+    end
+
+    -- compute distance between input and every dataset item
+    for i = 1, #dataset do
+    	local dist = getDistanceV(inputset, dataset[i])
+    	dataset[i][distcolumn] = round(dist, 2)		-- store the distance in the last column
+    -- print(dist)
+    end
+
+    -- sort the table based on the last column (distance)
+    table.sort(dataset, function(k1, k2) return k1[distcolumn] < k2[distcolumn] end)
+
+    -- print("Sorted dataset with distance")
+    -- print(inspect(dataset))
+
+    -- can now determine result
+    result = {}
+    -- add up the number of occurances in the top k data items
+    for i = 1, k do
+    	local label = dataset[i][labelcolumn]
+    	if result[label] == nil then result[label] = 0 end		-- initial result
+    	result[label] = result[label] + 1
+    end
+
+    -- print("*********")
+    -- print(inspect(result))
+
+    -- determine which label got the most 'hits'
+    table.sort(result, function(a, b) return a[1] < b[1] end)
+
+    -- print("*********")
+    -- print(inspect(result))
+
+    -- here is the prediction
+    -- print("************")
+    local maxlabelcount = 0
+    local maxlabelname = ""
+    -- print("Prediction")
+    for k, v in pairs(result) do
+    	if v > maxlabelcount then
+    		maxlabelcount = v
+    		maxlabelname = k
+    	end
+    end
+    -- print(maxlabelname, maxlabelcount)
+    -- print("************")
+
+    return maxlabelname, maxlabelcount
+
+end
+
+
+return knn
